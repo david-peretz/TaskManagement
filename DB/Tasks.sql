70db843e1465fd67d414eda6b46ab71593d8faaf
david82761 <david82761@gmail.com>
2024-06-25 08:58:37 -0700

    fix

---
+++ DB/Tasks.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Tasks] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [UserId]      INT            NOT NULL,
+    [Subject]     NVARCHAR (250) NOT NULL,
+    [TargetDate]  DATETIME       DEFAULT (getdate()) NOT NULL,
+    [IsCompleted] BIT            NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_Tasks_Users] FOREIGN KEY ([UserId]) REFERENCES [dbo].[Users] ([Id])
+);
+
---
+++ DB/Users.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]   INT            IDENTITY (1, 1) NOT NULL,
+    [Name] NVARCHAR (250) NOT NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
